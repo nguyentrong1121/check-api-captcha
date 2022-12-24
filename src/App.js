e1e26d8e5ec1cf2413e1d0aee0ec44c928d345f8
nguyentrong1121 <89018598+nguyentrong1121@users.noreply.github.com>
2022-12-24 13:02:16 +0700

    add read excel file

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import {useState} from "react";
 import axios from "axios";
 import moment from "moment";
+import * as xlsx from "xlsx";
+import {CopyToClipboard} from "react-copy-to-clipboard/src";
 
 function App() {
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState([])
     const [accounts, setAccouts] = useState([]);
     const check = async () => {
         try {
-            let temp = JSON.parse(input)
-            if (accounts) {
-                let lstAcc = await Promise.all(temp.map(async i => {
+            if (input?.length > 0) {
+                let lstAcc = await Promise.all(input.map(async i => {
                     let headers = {
                         apikey: i.apikey,
                         uid: i.uid
@@ -36,13 +37,33 @@ function App() {
     const formatHours = (h) => {
         return moment().startOf('day').add(h.replace(' Hours', ''), 'hours').format('HH:mm:ss');
     }
-    console.log(accounts)
+
+    const readUploadFile = (e) => {
+        e.preventDefault();
+        if (e.target.files) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const data = e.target.result;
+                const workbook = xlsx.read(data, { type: "array" });
+                const sheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[sheetName];
+                const json = xlsx.utils.sheet_to_json(worksheet);
+                setInput(json);
+            };
+            reader.readAsArrayBuffer(e.target.files[0]);
+        }
+    }
 
     return (
         <div className="App">
-            <textarea style={{height: 300}} onChange={(event) => {
-                setInput(event.target.value)
-            }}/>
+            <label htmlFor="upload">Upload File</label>
+            <input
+                type="file"
+                name="upload"
+                id="upload"
+                accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                onChange={readUploadFile}
+            />
             <button onClick={check}>
                 <p>lấy thông tin</p>
             </button>
@@ -57,10 +78,14 @@ function App() {
                                 alignItems: 'flex-start',
                                 marginBottom: 18,
                             }}>
-                                <p>{i.uid} - {i.used}: <span
-                                    style={{color: i.used < 200 ? 'black' : 'red'}}>{i.used < 200 ? 'OK' : 'TÈO'}</span>
-                                </p>
-                                <p>{i.apikey}</p>
+                                <CopyToClipboard text={i.uid}>
+                                    <p>{i.uid}-{200 - i.used}: <span
+                                        style={{color: i.used < 200 ? 'black' : 'red'}}>{i.used < 200 ? 'OK' : 'TÈO'}</span>
+                                    </p>
+                                </CopyToClipboard>
+                                <CopyToClipboard text={i.apikey}>
+                                    <p>{i.apikey}</p>
+                                </CopyToClipboard>
                                 <p>Reset sau: {formatHours(i.nextReset)}</p>
                             </div>
                         )
