984d3b8673ef01f4556d9274fe5a509cc3ab49a5
nguyentrong1121 <89018598+nguyentrong1121@users.noreply.github.com>
2022-12-28 08:56:29 +0700

    add more

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import axios from "axios";
 import moment from "moment";
 import * as xlsx from "xlsx";
 import {CopyToClipboard} from "react-copy-to-clipboard/src";
+import * as app from "react";
+import cors from "cors";
 
 function App() {
     const [input, setInput] = useState([])
     const [accounts, setAccouts] = useState([]);
+    const [gaga, setGaga] = useState([]);
+    const [params, setParams] = useState({
+        data: '{   "FixedAmountRequest": {     "recipient": "0x60c163504c5ba7fc8e462bd68612b7035aae5682"   } }',
+        cookie: '_ga=GA1.1.693361611.1671609123; _ga_0GW4F97GFL=GS1.1.1671609123.1.0.1671609127.56.0.0; _cfuvid=kAuKvSfh_g0sOuKbd5MbMOvfAkwsTLf0tj7iEv9DiZw-1672028267113-0-604800000'
+    });
     const check = async () => {
         try {
             if (input?.length > 0) {
@@ -17,7 +24,7 @@ function App() {
                         apikey: i.apikey,
                         uid: i.uid
                     }
-                    const response = await axios.get('https://free.nocaptchaai.com/api/account/balance', {
+                    const response = await axios.get('https://free.nocaptchaai.com/balance', {
                         headers
                     });
                     return {
@@ -26,6 +33,36 @@ function App() {
                     }
                 }))
                 setAccouts(lstAcc)
+            } else {
+                alert('Danh sách ít quá')
+            }
+        } catch (e) {
+            alert('Sai định dạng json')
+        }
+    }
+    const checkGaga = async () => {
+        try {
+            if (input?.length > 0) {
+                let lstGaga = await Promise.all(input.map(async i => {
+                    let headers = {
+                        Authorization: 'Bearer ' + i['GAGA-TOKEN']
+                    }
+                    const corsOrigin ={
+                        origin:'http://localhost:3000', //or whatever port your frontend is using
+                        credentials:true,
+                        optionSuccessStatus:200
+                    }
+                    app.use(cors(corsOrigin))
+                    const response = await axios.post('https://api.gaganode.com/api/node/query', {}, {
+                        headers
+                    });
+                    return {
+                        nodes: response.data.nodes,
+                        ...i
+                    }
+                }))
+                setGaga(lstGaga)
+                console.log(lstGaga)
             } else {
                 alert('Danh sách ít quá')
             }
@@ -44,7 +81,7 @@ function App() {
             const reader = new FileReader();
             reader.onload = (e) => {
                 const data = e.target.result;
-                const workbook = xlsx.read(data, { type: "array" });
+                const workbook = xlsx.read(data, {type: "array"});
                 const sheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[sheetName];
                 const json = xlsx.utils.sheet_to_json(worksheet);
@@ -54,43 +91,122 @@ function App() {
         }
     }
 
-    return (
-        <div className="App">
-            <label htmlFor="upload">Upload File</label>
-            <input
-                type="file"
-                name="upload"
-                id="upload"
-                accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-                onChange={readUploadFile}
-            />
-            <button onClick={check}>
-                <p>lấy thông tin</p>
-            </button>
-            <div>
-                {
-                    accounts.map(i => {
-                        return (
-                            <div style={{
+    const getGas = async () => {
+        let data = await axios.post('https://faucet.devnet.sui.io/gas', JSON.parse(params.data), {
+            Cookie: params.cookie,
+            'Content-Type': 'application/json'
+        })
+        console.log(data)
+    }
 
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'flex-start',
-                                marginBottom: 18,
-                            }}>
-                                <CopyToClipboard text={i.uid}>
-                                    <p>{i.uid}-{200 - i.used}: <span
-                                        style={{color: i.used < 200 ? 'black' : 'red'}}>{i.used < 200 ? 'OK' : 'TÈO'}</span>
-                                    </p>
-                                </CopyToClipboard>
-                                <CopyToClipboard text={i.apikey}>
-                                    <p>{i.apikey}</p>
-                                </CopyToClipboard>
-                                <p>Reset sau: {formatHours(i.nextReset)}</p>
-                            </div>
-                        )
-                    })
-                }
+    return (
+        <div>
+            <div className="App">
+                <label htmlFor="upload">Upload File</label>
+                <input
+                    type="file"
+                    name="upload"
+                    id="upload"
+                    accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                    onChange={readUploadFile}
+                />
+                <CopyToClipboard text={JSON.stringify(input)}>
+                    <button>
+                        <p>copy object</p>
+                    </button>
+                </CopyToClipboard>
+                <button onClick={check}>
+                    <p>lấy thông tin</p>
+                </button>
+                <div>
+                    {
+                        accounts.map(i => {
+                            return (
+                                <div style={{
+
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'flex-start',
+                                    marginBottom: 18,
+                                }}>
+                                    <CopyToClipboard text={i.uid}>
+                                        <p>{i.uid}-{200 - i.used}: <span
+                                            style={{color: i.used < 200 ? 'black' : 'red'}}>{i.used < 200 ? 'OK' : 'TÈO'}</span>
+                                        </p>
+                                    </CopyToClipboard>
+                                    <CopyToClipboard text={i.apikey}>
+                                        <p>{i.apikey}</p>
+                                    </CopyToClipboard>
+                                    <p>Reset sau: {i.nextReset}</p>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div className="App">
+                <button onClick={checkGaga}>
+                    <p>lấy thông tin</p>
+                </button>
+                <div>
+                    {
+                        gaga.map(i => {
+                            console.log(i)
+                            return !!i?.nodes && i.nodes.map(i => {
+                                return (
+                                    <div style={{
+
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'flex-start',
+                                        marginBottom: 18,
+                                    }}>
+                                        <CopyToClipboard text={i.uid}>
+                                            <p>{i.os}-{i.region_code}: {i.credit}
+                                            </p>
+                                        </CopyToClipboard>
+                                    </div>
+                                )
+                            })
+                        })
+                    }
+                </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <div className="App">
+                <input
+                    type="text"
+                    name="data"
+                    value={params.data}
+                    placeholder={'data'}
+                    onChange={(event) => {
+                        setParams(prevState => ({
+                            ...prevState,
+                            data: event.target.value
+                        }))
+                    }
+                    }
+                />
+                <input
+                    type="text"
+                    name="Cookie"
+                    value={params.cookie}
+                    placeholder={'cookie'}
+                    onChange={(event) => {
+                        setParams(prevState => ({
+                            ...prevState,
+                            cookie: event.target.value
+                        }))
+                    }
+                    }
+                />
+                <button onClick={getGas}>
+                    <p>lấy gas</p>
+                </button>
+                <div>
+                </div>
             </div>
         </div>
     );
---
+++ src/test.js
@@ -0,0 +1,426 @@
+// ==UserScript==
+// @name         hCaptcha Captcha Solver by noCaptchaAi
+// @name:ar      noCaptchaAI hCaptcha Solver حلال
+// @name:ru      noCaptchaAI Решатель капчи hCaptcha
+// @name:sh-CN   noCaptchaAI 验证码求解器
+// @namespace    https://nocaptchaai.com
+// @version      3.3.0
+// @description  hCaptcha Solver automated Captcha Solver bypass Ai service. Free 6000 🔥solves/month! 50x⚡ faster than 2Captcha & others
+// @description:ar تجاوز برنامج Captcha Solver الآلي لخدمة hCaptcha Solver خدمة Ai. 6000 🔥 حل / شهر مجاني! 50x⚡ أسرع من 2Captcha وغيرها
+// @description:ru hCaptcha Solver автоматизирует решение Captcha Solver в обход сервиса Ai. Бесплатно 6000 🔥решений/месяц! В 50 раз⚡ быстрее, чем 2Captcha и другие
+// @description:zh-CN hCaptcha Solver 自动绕过 Ai 服务的 Captcha Solver。 免费 6000 🔥解决/月！ 比 2Captcha 和其他人快 50x⚡
+// @author       noCaptcha AI and Diego
+// @match        *://*/*
+// @icon         https://docs.nocaptchaai.com/img/nocaptchaai.com.png
+// @require      https://greasyfork.org/scripts/395037-monkeyconfig-modern/code/MonkeyConfig%20Modern.js
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
+// @updateURL    https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js
+// @downloadURL  https://github.com/noCaptchaAi/hCaptchaSolver.user.js/raw/main/hCaptchaSolver.user.js
+// @grant        GM_info
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_openInTab
+// @grant        GM_addElement
+// @license      MIT
+// ==/UserScript==
+
+(async () => {
+    let startTime;
+    let stop;
+
+    const base = "https://free.nocaptchaai.com/api/";
+    const balUrl = base + "account/balance";
+    const baseUrl = base + "solve";
+
+    function log(msg) {
+        console.log(
+            "%cnoCaptchaAi.com ~ %c" + msg,
+            "background: #222; color: #bada55",
+            ""
+        );
+    }
+    (() => {
+        GM_addStyle(
+            `.swal2-container{
+           z-index:9990; }
+        `
+        );
+    })();
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        showCloseButton: true,
+        timer: 8000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
+
+    const cfg = new MonkeyConfig({
+        title: "⚙️noCaptchaAi.com All Settings",
+        menuCommand: true,
+        displayed: true,
+        overlay: true,
+        openLayer: true,
+        mode: "iframe",
+        params: {
+            OBJECT: {
+                type: "text",
+                default: "",
+            },
+            auto_solve: {
+                type: "checkbox",
+                default: true,
+            },
+            checkbox_auto_open: {
+                type: "checkbox",
+                default: false,
+            },
+            delay_before_checkbox_open: {
+                type: "number",
+                default: 200,
+            },
+            image_click_RandMin: {
+                type: "number",
+                default: 150,
+            },
+            image_click_RandMax: {
+                type: "number",
+                default: 250,
+            },
+            solve_puzzle_within_RandMin: {
+                type: "number",
+                default: 2000,
+            },
+            solve_puzzle_within_RandMax: {
+                type: "number",
+                default: 3000,
+            },
+            loop_run_interval: {
+                type: "number",
+                default: 1000,
+            },
+            debug_logs: {
+                type: "checkbox",
+                default: true,
+            },
+        },
+    });
+
+    let accounts = JSON.parse(cfg.get('OBJECT'));
+
+    let current = 0;
+
+    let headers = {
+        "Content-Type": "application/json",
+        // get apikey http://nocaptchaai.com
+        uid: accounts[0].uid,
+        apikey: accounts[0].apikey,
+    };
+
+    log(
+        "auto open= " +
+        cfg.get("checkbox_auto_open") +
+        " | " +
+        "auto solve= " +
+        cfg.get("auto_solve") +
+        " | " +
+        "loop running in bg"
+    );
+
+    if (document.querySelector("#warning")) {
+        const div = document.createElement("div");
+        div.onclick = function (e) {
+            e.preventDefault();
+            cfg.open("window", {
+                windowFeatures: {
+                    width: 500,
+                },
+            });
+        };
+        div.style =
+            "color: rgb(235, 87, 87); font-size: 10px; bottom: 5px; left: 5px; width: 75%; position: absolute;";
+        div.innerHTML =
+            "<code>apikey</code> or <code>uid</code> not set. Click here to set up";
+        document.querySelector("#warning").insertAdjacentElement("afterEnd", div);
+    }
+    GM_registerMenuCommand("💲 Check Balance xmlHttp ", function () {
+        GM_xmlhttpRequest({
+            method: "GET",
+            headers,
+            url: balUrl,
+            onload: function (response) {
+                log(response.responseText);
+                log(response.status === 200);
+                if (response) {
+                    Toaster(
+                        "success",
+                        "<b>noCaptchaAi.com ~</b><i> Balance:-</i>",
+                        response.responseText
+                    );
+                }
+            },
+        });
+    });
+
+    GM_registerMenuCommand("🏠 HomePage", function () {
+        GM_openInTab("https://nocaptchaai.com", {
+            active: true,
+            setParent: true,
+        });
+    });
+
+    GM_registerMenuCommand("📈 Dashboard", function () {
+        GM_openInTab("https://dash.nocaptchaai.com", {
+            active: true,
+            setParent: true,
+        });
+    });
+    GM_registerMenuCommand("💸 Buy Solving Quota", function () {
+        GM_openInTab("https://nocaptchaai.com/buy.html", {
+            active: true,
+            setParent: true,
+        });
+    });
+    GM_registerMenuCommand("📄 Api Docs", function () {
+        GM_openInTab("https://docs.nocaptchaai.com/category/api-methods", {
+            active: true,
+            setParent: true,
+        });
+    });
+    GM_registerMenuCommand("📄 Github", function () {
+        GM_openInTab("https://github.com/shimuldn/hCaptchaSolverApi", {
+            active: true,
+            setParent: true,
+        });
+    });
+    GM_registerMenuCommand("❓ Discord", function () {
+        GM_openInTab("https://discord.gg/E7FfzhZqzA", {
+            active: true,
+            setParent: true,
+        });
+    });
+    GM_registerMenuCommand("❓ Telegram", function () {
+        GM_openInTab("https://t.me/noCaptchaAi", {
+            active: true,
+            setParent: true,
+        });
+    });
+
+    log("Run done");
+
+    while (true) {
+        if (!navigator.onLine) break;
+        if (stop) break;
+
+        await sleep(cfg.get("loop_run_interval"));
+
+        if (cfg.get("checkbox_auto_open") && isWidget()) {
+            const isSolved =
+                document.querySelector("div.check")?.style.display === "block";
+            if (isSolved && cfg.get("debug_logs")) log("found solved");
+            if (isSolved) break;
+            await sleep(cfg.get("delay_before_checkbox_open"));
+            document.querySelector("#checkbox")?.click();
+        } else if (
+            cfg.get("auto_solve") &&
+            document.querySelector("h2.prompt-text") !== null
+        ) {
+            await solve();
+            if (cfg.get("debug_logs") === true) log("opening box");
+        }
+    }
+
+    function isWidget() {
+        const rect = document.body.getBoundingClientRect();
+        if (rect?.width === 0 || rect?.height === 0) {
+            return false;
+        }
+        return document.querySelector("div.check") !== null;
+    }
+
+    function sleep(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    function randTimer(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function Toaster(status, text, response) {
+        Toast.fire({
+            target: "body",
+            icon: status,
+            title: text + "\n" + response,
+            timer: 10000,
+            width: "90%",
+            timerProgressBar: true,
+            color: "#222",
+            grow: "row",
+            background: "#fff",
+            padding: "4em",
+            showCloseButton: true,
+        });
+    }
+
+    async function getBase64FromUrl(url) {
+        const blob = await (await fetch(url)).blob();
+        return new Promise(function (resolve) {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob);
+            reader.addEventListener("loadend", function () {
+                resolve(reader.result.replace(/^data:image\/(png|jpeg);base64,/, ""));
+            });
+            reader.addEventListener("error", function () {
+                log("❌ Failed to convert url to base64");
+            });
+        });
+    }
+
+    async function multiple() {
+        current++;
+        let user = accounts[current];
+        headers = {
+            "Content-Type": "application/json",
+            // get apikey http://nocaptchaai.com
+            uid: user.uid,
+            apikey: user.apikey,
+        };
+    }
+
+    async function solve() {
+        const { target, cells, images } = await on_task_ready();
+
+        if (!cfg.get("auto_solve")) {
+            return;
+        }
+        startTime = new Date();
+
+        const searchParams = new URLSearchParams(location.hash);
+
+        try {
+            let response = await fetch(baseUrl, {
+                method: "POST",
+                headers,
+                body: JSON.stringify({
+                    images,
+                    target,
+                    method: "hcaptcha_base64",
+                    sitekey: searchParams.get("sitekey"),
+                    site: searchParams.get("host"),
+                    ln: document.documentElement.lang || navigator.language,
+                    softid: "UserScript",
+                }),
+            });
+
+            response = await response.json();
+            if (cfg.get("debug_logs") === true) log("sent for solving");
+            log("🕘 waiting for response");
+
+            if (response.status === "new") {
+                if (cfg.get("debug_logs") === true) console.table(response);
+                log("waiting 2s");
+                await sleep(2000);
+                const status = await (await fetch(response.url)).json();
+                for (const index of status.solution) {
+                    cells[index].click();
+                }
+            } else if (response.status === "solved") {
+                if (cfg.get("debug_logs") === true) console.table(response);
+                if (cfg.get("debug_logs") === true)
+                    console.log(
+                        "noCaptchaAi.com ~ ☑️ server procssed in",
+                        response.processing_time
+                    );
+
+                log("🖱️ -> 🖼️");
+                const randmin = parseInt(cfg.get("image_click_RandMin"));
+                const randmax = parseInt(cfg.get("image_click_RandMax"));
+                for (const index of response.solution) {
+                    cells[index].click();
+                    await sleep(randTimer(randmin, randmax));
+                }
+            } else if (response.status === "Unauthorized") {
+                Toaster(
+                    "error",
+                    "noCaptchaAi.com Apikey or uid not valid. \n Popup window will open, if blocked enable and refresh",
+                    response.status
+                );
+                await sleep(2000);
+                cfg.open("window");
+                return log(response.status, response.message);
+            } else if (response.error.match("restricted")) {
+                Toaster(
+                    "error",
+                    "<h3>Account flagged</h3> <h4>a) Buy Plan</h4> <h4>b) Ping on Discord/Telegram for activation</h4> ",
+                    response.error
+                );
+                stop = true;
+                await sleep(3000);
+                window.open("https://nocaptchaai.com/buy.html");
+            } else if (response.error) {
+                Toaster("error", response.error);
+                await multiple();
+                await sleep(3000);
+            } else {
+                Toaster(response.status);
+                return log(response.status);
+            }
+
+            let randmin = parseInt(cfg.get("solve_puzzle_within_RandMin"));
+            let randmax = parseInt(cfg.get("solve_puzzle_within_RandMax"));
+            // console.log(randTimer(randmin, randmax));
+            // console.log("rand", typeof randmax, randmax, randmin);
+
+            const elapsedTime = new Date() - startTime;
+            // console.log("elapsed", elapsedTime);
+            // TODO
+            const remainingTime = randTimer(randmin, randmax) - elapsedTime;
+            // console.log("remaining", remainingTime);
+            await sleep(remainingTime);
+            if (cfg.get("debug_logs") === true) log("waiting random time");
+            // await sleep(cfg.get("delay_before_submit"));
+            log("☑️ sent!");
+            document.querySelector(".button-submit").click();
+            startTime = 0;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    function on_task_ready(i = 500) {
+        return new Promise(async (resolve) => {
+            const check_interval = setInterval(async function () {
+                let target = document.querySelector(".prompt-text")?.textContent;
+                if (!target) return;
+
+                const cells = document.querySelectorAll(".task-image .image");
+                if (cells.length !== 9) return;
+
+                const images = {};
+                for (let i = 0; i < cells.length; i++) {
+                    const img = cells[i];
+                    if (!img) return;
+                    const url = img.style.background.match(/url\("(.*)"/)?.at(1) || null;
+                    if (!url || url === "") return;
+                    images[i] = await getBase64FromUrl(url);
+                }
+
+                clearInterval(check_interval);
+                return resolve({
+                    target,
+                    cells,
+                    images,
+                });
+            }, i);
+        });
+    }
+})();
